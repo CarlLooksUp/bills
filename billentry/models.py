4aa131f4fa9ee381c6a5bb5cb3a46286026c51a1
Carl Morrissey <carl.morrissey@gmail.com>
2012-10-24 20:59:40 -0400

    Renamed models, added utility functions.
    Renamed models to Payment/Receipt. Payments can be used to reconstruct payment
    events, and receipts can be used to tally debts. Utility functions allow
    conversion to integer monetary values. Also enabled admin access for bills
    models.

--- billentry/models.py
+++ billentry/models.py
@@ -1,15 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 
-# Create your models here.
 
-class Purchase(models.Model):
+class Payment(models.Model):
     date = models.DateTimeField('date of purchase')    
     notes = models.CharField(max_length=300)
     payer = models.ForeignKey(User)
     total = models.IntegerField()
 
-class Transaction(models.Model):
+    def __unicode__(self):
+        return self.payer.get_full_name() + " paid " + self.get_total() + " for " + self.notes
+
+    def get_total(self): #Extend IntegerField to handle currency?
+        return self.total/100.0
+
+    def set_total(self, new_total):
+        self.total = int(new_total * 100)
+
+
+class Receipt(models.Model):
     debtor = models.ForeignKey(User)
     amount = models.IntegerField()
-    purchase = models.ForeignKey(Purchase)
+    purchase = models.ForeignKey(Payment)
+
+    def __unicode__(self):
+        return (self.debtor.get_full_name() + " owes " + self.get_amount() + " to " + 
+                self.purchase.payer.get_full_name())
+    
+    def get_amount(self): #Extend IntegerField to handle currency?
+        return self.amount/100.0
+
+    def set_total(self, new_amount):
+        self.amount = int(new_amount * 100)
