9882baced7f601b14a0a5e81581413ba72a5a304
admiralteal <admiral.teal@gmail.com>
2012-11-05 10:47:51 -0500

    Added add forms.  Need deletion ability?

--- bare html mockups/scripts.js
+++ bare html mockups/scripts.js
@@ -3,29 +3,41 @@
 /*
  * This script builds the Payments notes expansion by finding 
  * rows in the Payments table that are followed by rows with
- * the class 'notes'.  Clicking the row expands the note.
- * Clicking the note collapses it again.
+ * the class 'notes'.
  */
+ 
 function prepareExpander() {
 	if (document.getElementById('Payments')) {
 		var paymentsTable = document.getElementById('Payments');
 		var rows = paymentsTable.getElementsByTagName('tr');
 		for (var i=0; i < rows.length; i++ ) {
-			if (rows[i].className != 'notes') {
+			if (rows[i].className == 'notes') {
+				rows[i].onclick = function() {
+					this.style.display = 'none';
+				}
+			} else {
 				rows[i].onclick = function() {
 					nextRow = this.nextSibling;
 					while(nextRow && nextRow.nodeType != 1) {
 						nextRow = nextRow.nextSibling
 					}
-					nextRow.style.display = 'table-row';
-				}
-			} else {
-				rows[i].onclick = function() {
-					this.style.display = 'none';
+					if (nextRow.style.display != 'table-row') {
+						nextRow.style.display = 'table-row';
+					} else {
+						nextRow.style.display = 'none';
+					}
 				}
 			}
 		}
 	}
 }
 
-window.onload(prepareExpander);
+function hideForms() {
+	document.getElementById('addPayment').style.display = 'none';
+	document.getElementById('addReciept').style.display = 'none';
+}
+
+function showForm(/* String */ form) {
+	hideForms();
+	document.getElementById(form).style.display = 'block';
+}
