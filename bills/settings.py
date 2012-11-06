d4b1e34e3434c0a66debb976d4ab94fce7dfa952
Carl Morrissey <carl.morrissey@gmail.com>
2012-11-06 15:18:36 -0500

    Add ajax submodule and login pages
    Installed django-dajax for ajax support. Module designed for use with jQuery
    (or similar framework). Added login page templates tied into mockup css.

--- bills/settings.py
+++ bills/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'billentry',
+    'django-dajax',
 )
 
 # A sample logging configuration. The only tangible logging
--- bills/urls.py
+++ bills/urls.py
@@ -14,5 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^login/', 'django.contrib.auth.views.login', {'template_name': 'billentry/login.html'}), 
+    (r'login/?$', 'django.contrib.auth.views.login', 
+                {'template_name': 'user/login.html'}), 
+    (r'logout/?$', 'django.contrib.auth.views.logout', 
+                {'template_name': 'user/logout.html'}),
 )
