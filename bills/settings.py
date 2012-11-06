5f04aa5b015e6ce31bdc2410b797327095f15872
Carl Morrissey <carl.morrissey@gmail.com>
2012-11-06 15:46:04 -0500

    Fix dajax import and css serving

--- bills/settings.py
+++ bills/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'billentry',
-    'django-dajax',
+    'dajax',
 )
 
 # A sample logging configuration. The only tangible logging
